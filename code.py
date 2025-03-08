e41c114a74b3153521e4a8a83a99f34550a3adbe
hannahilea <hannahilea@users.noreply.github.com>
2025-03-07 21:47:04 -0500

    set up for tetris

--- code.py
+++ code.py
@@ -2,103 +2,85 @@
 """
 BongoBoard controller that acts as a BLE HID keyboard to peer devices.
 """
-import board # type: ignore 
-import time
+import board  # type: ignore
+# import time
 from digitalio import DigitalInOut, Direction
 from digitalio import Pull
-from analogio import AnalogIn
 
-# import adafruit_ble
-# from adafruit_ble.advertising import Advertisement
-# from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
-# from adafruit_ble.services.standard.hid import HIDService
-# from adafruit_ble.services.standard.device_info import DeviceInfoService
+import adafruit_ble
+from adafruit_ble.advertising import Advertisement
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.standard.hid import HIDService
+from adafruit_ble.services.standard.device_info import DeviceInfoService
 
-# Set up hardware
-class BongoButtons:
-    def __init__(self):
-        self.left_top = DigitalInOut(board.D10)
-        self.left_bottom = DigitalInOut(board.D9)
-        self.right_top = DigitalInOut(board.D6)
-        self.right_bottom = DigitalInOut(board.D5)
-        self.buttons = [self.left_top, self.left_bottom, self.right_top, self.right_bottom]
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
 
-        # Set up buttons 
-        for b in self.buttons:
-            b.direction = Direction.INPUT 
-            b.pull = Pull.UP
-
-        # Set up button state
-        self.last_polled_state = [not b.value for b in self.buttons]
-
-    def __str__(self):
-        return f"{[b.value for b in self.buttons]}"
-
-    def get_current_button_status(self):
-        updated_status = ["" for _ in self.buttons]
-        current_state = [not b.value for b in self.buttons] 
-        any_changed = False
-        for i in range(len(self.buttons)):
-            # print(i, self.last_polled_state, current_state)
-            if current_state[i] == self.last_polled_state[i]:
-                updated_status[i] = "pressed" if current_state[i] else "released"
-            elif current_state[i] == True:
-                updated_status[i] = "Newly pressed" 
-                any_changed = True
-            else:
-                updated_status[i] = "Newly released" 
-                any_changed = True
-        self.last_polled_state = current_state 
-        return any_changed, updated_status
-
-BONGO_BUTTONS = BongoButtons()
-print("Instantiated BongoBoard!", BONGO_BUTTONS)
-
-MIC = AnalogIn(board.A1)
-NUM_SMOOTHED_MIC_INPUTS = 1000
-MIC_BUFFER = [0 for _ in range(NUM_SMOOTHED_MIC_INPUTS)]
 
 # Set up bluetooth
-# hid = HIDService()
-# device_info = DeviceInfoService(
-#     software_revision=adafruit_ble.__version__, manufacturer="Adafruit Industries"
-# )
-# advertisement = ProvideServicesAdvertisement(hid)
-# advertisement.appearance = 961
-# scan_response = Advertisement()
-# scan_response.complete_name = "BongoBoard"
+hid = HIDService()
+device_info = DeviceInfoService(
+    software_revision=adafruit_ble.__version__,
+    manufacturer="Adafruit Industries"
+)
+advertisement = ProvideServicesAdvertisement(hid)
+advertisement.appearance = 961
+scan_response = Advertisement()
+scan_response.complete_name = "BongoBoard"
 
-# ble = adafruit_ble.BLERadio()
-# ble.name = "BongoBoard"
-# if not ble.connected:
-#     print("advertising")
-#     ble.start_advertising(advertisement, scan_response)
-# else:
-#     print("already connected")
-#     print(ble.connections)
+ble = adafruit_ble.BLERadio()
+ble.name = "BongoBoard"
+if not ble.connected:
+    print("advertising")
+    ble.start_advertising(advertisement, scan_response)
+else:
+    print("already connected")
+    print(ble.connections)
+
+k = Keyboard(hid.devices)
+kl = KeyboardLayoutUS(k)
+
+# Set up hardware
+class BongoButton:
+    def __init__(self, name, button_pin, button_pushed_key):
+        self.button = DigitalInOut(button_pin)
+        self.button.direction = Direction.INPUT
+        self.button.pull = Pull.UP
+        self.last_polled_state = not self.button.value
+        self.button_pushed_key = button_pushed_key
+        self.name = name
+
+    def __str__(self):
+        return f"{self.button.value}"
+
+    def handle(self):
+        current_state = not self.button.value
+        if (current_state != self.last_polled_state):
+            if current_state:
+                # print(self.name, " pushed! Triggering ", self.button_pushed_key)
+                k.send(self.button_pushed_key)
+            self.last_polled_state = current_state
 
 
-# Some development constants that can probably be removed soonish!
-MONITOR_MIC = False  # Keep MIC buffer up to date?
-VERBOSE = True # Show assorted logging/debugging statements?
+BONGO_BUTTONS = [BongoButton("left_top", board.D10, Keycode.DOWN_ARROW),
+                 BongoButton("left_bottom", board.D9, Keycode.UP_ARROW),
+                 BongoButton("right_top", board.D6, Keycode.RIGHT_ARROW),
+                 BongoButton("right_bottom", board.D5, Keycode.LEFT_ARROW)]
+print("Instantiated BongoBoard!", BONGO_BUTTONS)
+
 
 def main_event_loop():
+    bt_connected = False
     while True:
-        # while not ble.connected:
-        #     continue
-        # print("Bluetooth connected!")
+        while not ble.connected:
+            continue
+        if not bt_connected:
+            print("Bluetooth connected!")
+            bt_connected = True
 
-        if MONITOR_MIC:
-            MIC_BUFFER.append(MIC.value)
-            MIC_BUFFER.pop(0)
-            if VERBOSE:
-                abs_max = max([abs(x) for x in MIC_BUFFER])
-                print(">mic_abs_max:",abs_max,",neg_abs_max:",-abs_max,",mic_raw:", MIC_BUFFER[NUM_SMOOTHED_MIC_INPUTS - 1],"\r\n")
-        
-        any_changed, button_state = BONGO_BUTTONS.get_current_button_status()
-        if any_changed:
-            print(button_state)
-
+        for button in BONGO_BUTTONS:
+            button.handle()
 
 if __name__ == "__main__":
     main_event_loop()
